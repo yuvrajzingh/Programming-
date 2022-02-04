7dcc0254be80b475a78e805b1e918d0a2a17b428
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-02-04 17:22:45 +0530

    Add files via upload

---
+++ decimal-to-binary.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int decimalToBinary (int n)
+{
+    int x = 1;
+    int ans = 0;
+    while(x<=n)
+        x*= 2;
+    x/=2;    
+    
+
+    while(x>0)
+    {
+        int lastDigit = n/x;
+        n -= lastDigit*x;
+        x/=2;
+        ans = ans*10 + lastDigit;
+
+    }
+    return ans;
+}
+
+
+int main()
+{
+    int n;
+    
+    cout<<"Enter the decimal number : ";
+    cin>>n;
+
+    cout<< "The binary form of the above number is: "<<decimalToBinary(n);
+
+    return 0;
+}
