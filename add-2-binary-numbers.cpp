2150b75e02071814128b25f29da887364beeead2
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-02-06 23:55:10 +0530

    Update add-2-binary-numbers.cpp

--- add-2-binary-numbers.cpp
+++ add-2-binary-numbers.cpp
@@ -1,28 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string addBinaryNumbers(string a, string b)
+{
+   string res;
+   int i= a.size() -1, j = b.size() - 1;
+   int sum, carry = 0;
+   while(i>=0 || j>=0)
+   {
+      sum = carry;
+      if(i >= 0)
+       sum += a[i] - '0';
+      if(j >= 0)
+       sum+= b[j] - '0';
+      res += to_string(sum%2);
+      carry = sum/2;
+      i--, j--; 
 
-//function to add two strings
-string add(string a, string b){
-   string result = "";
-   int temp = 0;
-   int size_a = a.size() - 1;
-   int size_b = b.size() - 1;
-   while (size_a >= 0 || size_b >= 0 || temp == 1){
-      temp += ((size_a >= 0)? a[size_a] - '0': 0);
-      temp += ((size_b >= 0)? b[size_b] - '0': 0);
-      result = char(temp % 2 + '0') + result;
-      temp /= 2;
-      size_a--; size_b--;
    }
-   return result;
-}
-int main(){
-   string a, b;
+   if(carry != 0)res += '1';
+   reverse(res.begin(), res.end());
+   
+   return res;
 
-   cout<< "Enter the two binary numbers you want to add: "<<endl; 
-   cin>> a;
-   cin>> b;
-   cout<<"sum of strings are : "<<add(a, b);
+}
+
+
+int main()
+{
+   string a, b;
+   cout<<"Enter two binary numbers that you want to add: "<<endl;
+   cin>>a;
+   cin>>b;
+
+   cout<<"Addition of the above binary numbers are: "<<addBinaryNumbers(a, b);
+
+
+
    return 0;
 }
+
