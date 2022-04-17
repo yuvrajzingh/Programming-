639abb58dc3e85e4fbe0f50f46930b82349c5cdf
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-04-17 23:22:28 +0530

    Add files via upload

---
+++ recursion-increasing-decreasing.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void dec(int n)
+{
+    if(n==0)
+    {
+        return ;
+    }
+
+    cout<<n<<endl;
+    dec(n-1);
+
+}
+
+void inc(int n)
+{
+    if(n==0)
+    {
+        return ;
+    }
+
+
+    inc(n-1);
+    cout<<n<<endl;
+}
+
+
+
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    cout<<"For decreasing: "<<endl;
+    dec(n);
+
+    cout<<"For increasing: "<<endl;
+    inc(n);
+
+
+    return 0;
+}
