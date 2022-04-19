15e71b257cde01fd255aa6e055467a784e356fce
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-04-19 23:26:03 +0530

    Add files via upload

---
+++ Recursion-tower-of-Hanoi.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void towerOfHanoi(int n, char src, char dest, char helper)
+{
+    if(n==0)
+    {
+        return ;   // base case
+    }
+
+    towerOfHanoi(n-1, src, helper, dest);
+    cout<<"Move from "<<src<<" to "<<dest<<endl;
+    towerOfHanoi(n-1, helper, dest, src);
+}
+
+
+
+int main()
+{
+    towerOfHanoi(3, 'A', 'B', 'C');
+
+    return 0;
+}
