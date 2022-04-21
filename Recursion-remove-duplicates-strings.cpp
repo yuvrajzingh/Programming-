b3a14b30309aa0ab2643975407d1803264615e2a
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-04-21 23:03:17 +0530

    Add files via upload

---
+++ Recursion-remove-duplicates-strings.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+string removeDuplicates(string s)
+{
+    if(s.length()==0)
+    {
+        return " ";
+    }
+
+    char ch = s[0];
+    
+    string ans = removeDuplicates(s.substr(1));
+
+    if(ch == ans[0])
+    {
+        return ans;
+    }
+
+    return (ch+ans);
+
+}
+
+
+int main()
+{
+
+    cout<<removeDuplicates("babblingbaboon");
+
+
+    return 0;
+}
