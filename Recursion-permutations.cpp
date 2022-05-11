28677a768bd5c8d169f17c73d5ef044d3ee16067
Yuvraj Singh Bhadouria <88030872+yuvrajzingh@users.noreply.github.com>
2022-05-11 23:18:27 +0530

    Add files via upload

---
+++ Recursion-permutations.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void permutations(string s, string ans)
+{
+    if(s.length()==0)      //base case
+    {
+        cout<<ans<<endl;
+        return;
+    }
+
+    for(int i=0; i<s.length(); i++)
+    {
+        char ch = s[i];
+        string ros = s.substr(0, i) + s.substr(i+1);
+
+        permutations(ros, ans+ch);
+    }
+
+}
+
+
+
+int main()
+{
+
+    permutations("ABC", "");
+
+    return 0;
+}
